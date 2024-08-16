09b3c7d72a81eba4f68230f36428634f21f6a2fc
sashakter <sashakter@gmail.com>
2024-08-16 22:20:20 +0200

    add styles about company

--- src/app/components/AboutCompany.tsx
+++ src/app/components/AboutCompany.tsx
@@ -4,7 +4,19 @@ import Title from "./Title";
 const AboutCompany: React.FC = () => {
   return (
     <div className="py-10 relative">
-        <div className="absolute top-0 left-0 w-full h-full bg-gray-100"><</div>
+    <div className="absolute inset-0 z-0">
+        <Image
+          src={'/about-com-bg.jpg'}
+          alt="background photo"
+          fill={true}
+          className="object-cover"
+          quality={50}
+        />
+      </div>
+
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-[#000] opacity-90 z-0"></div>
+      <div className="container mx-auto p-4 md:p-6 lg:p-8 relative z-10">
       <Title title="ПРО КОМПАНІЮ"/>
       <div className="flex flex-col items-center lg:flex-row justify-around">
         <Image
@@ -13,7 +25,8 @@ const AboutCompany: React.FC = () => {
           width={350}
           height={350}
         />
-        <Image src={"/EarLine.svg"} alt="ear line" width={26} height={344} />
+        <Image src={"/EarLine.svg"} className="w-8 max-lg:rotate-90 max-md:w-4" alt="ear line" width={344} height={26} />
+        <div className="flex flex-col items-center lg:items-start">
         <ul className="list-none text-2xl max-w-md flex flex-col gap-5">
           <li className="flex items-center gap-5">
             <Image className="shadow-sm" src={"/ear.svg"} alt="ear" width={24} height={24} />
@@ -37,9 +50,11 @@ const AboutCompany: React.FC = () => {
             горілки особливі, солодкі та гіркі настоянки.
           </li>
           <li>
-            <button className="bg-white text-black px-5 py-2 uppercase rounded-lg">Дізнатися БІЛЬШЕ</button>
           </li>
         </ul>
+            <button className="bg-white text-2xl text-black px-5 py-2 uppercase rounded-lg">дізнатися більше</button>
+        </div>
+      </div>
       </div>
     </div>
   );
