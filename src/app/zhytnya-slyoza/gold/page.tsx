c2e1599fc84cecfafe4f737cd3e949245171713f
RomanHav <roman14082004@gmail.com>
2024-10-02 02:11:19 +0300

    fix images

--- src/app/zhytnya-slyoza/gold/page.tsx
+++ src/app/zhytnya-slyoza/gold/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React from 'react'
 import Image from 'next/image'
-import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import 'swiper/css/navigation'
