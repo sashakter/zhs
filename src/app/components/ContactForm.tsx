36d0d1fe65cd8ecaf9f7a96e7058479ba9a5752b
RomanHav <roman14082004@gmail.com>
2024-10-03 19:32:00 +0300

    fix contact form

--- src/app/components/ContactForm.tsx
+++ src/app/components/ContactForm.tsx
@@ -1,9 +1,15 @@
 'use client'
-import { Field, Form, Formik, FormikHelpers } from 'formik'
+import axios from 'axios'
+import { ErrorMessage, Field, Form, Formik, FormikHelpers } from 'formik'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 import { MdCall } from 'react-icons/md'
 import { MdEmail } from 'react-icons/md'
+import { FaInstagram } from 'react-icons/fa'
 import { MdLocationPin } from 'react-icons/md'
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
+import * as Yup from 'yup'
 
 interface ContactProps {
   name: string
@@ -22,6 +28,31 @@ const Contacts: React.FC = () => {
     surname: '',
   }
 
+  const [location, setLocation] = useState('')
+
+  useEffect(() => {
+    const fetchLocation = async () => {
+      try {
+        const response = await axios.get('https://ipapi.co/json/')
+        setLocation(response.data.country_code)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchLocation()
+  }, [])
+
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required('Обовʼязкове поле'),
+    surname: Yup.string().min(3, 'Введіть коректні дані'),
+    email: Yup.string()
+      .email('Невірний формат пошти')
+      .required('Обовʼязкове поле'),
+    phone: Yup.string().required('Обовʼязкове поле'),
+    message: Yup.string(),
+  })
+
   const handleSubmit = (
     values: ContactProps,
     actions: FormikHelpers<ContactProps>,
@@ -34,7 +65,7 @@ const Contacts: React.FC = () => {
     <div className="flex flex-col gap-10 py-24 text-black">
       <div className="flex items-center justify-center gap-24">
         <div className="flex flex-col items-center gap-12 text-lg">
-          <div className="flex w-[325px] items-center gap-5 rounded-lg bg-custom-contact py-2 pl-4 pr-10">
+          <div className="bg-custom-contact flex w-[325px] items-center gap-5 rounded-lg py-2 pl-4 pr-10">
             <div className="rounded-full bg-white p-2">
               <MdCall size={40} />
             </div>
@@ -43,7 +74,7 @@ const Contacts: React.FC = () => {
               <a href="tel:+380(67)-706-68-47">+380(67)-706-68-47</a>
             </div>
           </div>
-          <div className="flex w-[325px] items-center gap-5 rounded-lg bg-custom-contact py-2 pl-4 pr-10">
+          <div className="bg-custom-contact flex w-[325px] items-center gap-5 rounded-lg py-2 pl-4 pr-10">
             <div className="rounded-full bg-white p-2">
               <MdEmail size={40} />
             </div>
@@ -54,7 +85,7 @@ const Contacts: React.FC = () => {
               </a>
             </div>
           </div>
-          <div className="flex w-[325px] items-center gap-5 rounded-lg bg-custom-contact py-2 pl-4 pr-10">
+          <div className="bg-custom-contact flex w-[325px] items-center gap-5 rounded-lg py-2 pl-4 pr-10">
             <div className="rounded-full bg-white p-2">
               <MdLocationPin size={40} />
             </div>
@@ -63,55 +94,121 @@ const Contacts: React.FC = () => {
               <a> м. Київ</a>
             </div>
           </div>
+          <div className="bg-custom-contact flex w-[325px] items-center gap-5 rounded-lg py-2 pl-4 pr-10">
+            <div className="rounded-full bg-white p-2">
+              <FaInstagram size={40} />
+            </div>
+            <div className="flex flex-col justify-center gap-1 text-white">
+              <span className="text-2xl">Instagram</span>
+              <a href="https://www.instagram.com/zhitnia_sloza_?igsh=MWN0NW43cDBtaWg4bw==">
+                zhitnia_sloza
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="rounded-lg bg-custom-contact p-9 xl:max-w-[600px]">
-          <h4 className="text-2xl text-white mb-3 uppercase">Контактна Форма</h4>
-          <p className="text-base text-white mb-6 tracking-wide">
+        <div className="bg-custom-contact rounded-lg p-9 xl:max-w-[600px]">
+          <h4 className="mb-3 text-2xl uppercase text-white">
+            Контактна Форма
+          </h4>
+          <p className="mb-6 text-base tracking-wide text-white">
             Заповніть необхідну інформацію, щоб ми мали змогу надати вам швидку
             відповідь!
           </p>
-          <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-            <Form className="flex flex-col items-start gap-5">
-              <div className="flex gap-4 w-full">
-                <Field
-                  className="w-full rounded-md border border-white text-white bg-inherit px-2 py-3"
-                  name="name"
-                  type="text"
-                  placeholder="Ім&#39;я"
-                />
-                <Field
-                  className="w-full rounded-md border border-white text-white bg-inherit px-2 py-3"
-                  name="surname"
-                  type="text"
-                  placeholder="Прізвище"
-                />
-              </div>
-              <div className="flex gap-4 w-full">
-                <Field
-                  className="w-full rounded-md border border-white text-white bg-inherit px-2 py-3"
-                  name="email"
-                  type="email"
-                  placeholder="Пошта"
-                />
-                <Field
-                  className="w-full rounded-md border border-white text-white bg-inherit px-2 py-3"
-                  name="phone"
-                  type="text"
-                  placeholder="Номер телефону"
-                />
-              </div>
-              <Field
-                className="w-full rounded-md border border-white text-white bg-inherit px-2 py-3"
-                name="message"
-                as="textarea"
-                placeholder="Коментар"
-                columns={8}
-                rows={5}
-              />
-              <button className="rounded-md bg-custom-contactbtn tracking-wide px-6 py-2 text-2xl font-medium text-white hover:bg-white hover:text-black hover:duration-700">
-                Надіслати
-              </button>
-            </Form>
+          <Formik
+            initialValues={initialValues}
+            onSubmit={handleSubmit}
+            validationSchema={validationSchema}
+          >
+            {({ setFieldValue, values }) => (
+              <Form className="flex flex-col items-start gap-5">
+                <div className="flex w-full gap-4">
+                  <div className="flex w-full flex-col">
+                    <Field
+                      className="h-[50px] w-full rounded-md border border-white bg-inherit px-2 py-3 text-white"
+                      name="name"
+                      type="text"
+                      placeholder="Ім&#39;я"
+                    />
+                    <ErrorMessage
+                      className="text-red-600"
+                      name="name"
+                      component={'span'}
+                    />
+                  </div>
+                  <div className="flex w-full flex-col">
+                    <Field
+                      className="h-[50px] w-full rounded-md border border-white bg-inherit px-2 py-3 text-white"
+                      name="surname"
+                      type="text"
+                      placeholder="Прізвище"
+                    />
+                    <ErrorMessage
+                      className="text-red-600"
+                      name="surname"
+                      component={'span'}
+                    />
+                  </div>
+                </div>
+                <div className="flex w-full gap-4">
+                  <div className="flex w-full flex-col">
+                    <Field
+                      className="h-[50px] w-full rounded-md border border-white bg-inherit px-2 py-3 text-white"
+                      name="email"
+                      type="email"
+                      placeholder="Пошта"
+                    />
+                    <ErrorMessage
+                      className="text-red-600"
+                      name="email"
+                      component={'span'}
+                    />
+                  </div>
+                  <div className="flex w-full flex-col">
+                    <PhoneInput
+                      country={location.toLowerCase()}
+                      value={values.phone}
+                      onChange={(phone) => setFieldValue('phone', phone)}
+                      inputProps={{
+                        name: 'phone',
+                        required: true,
+                      }}
+                      inputStyle={{
+                        width: '100%',
+                        paddingLeft: '50px',
+                        borderRadius: '6px',
+                        height: '50px',
+                        backgroundColor: '#242322',
+                        color: '#fff',
+                      }}
+                    />
+                    <ErrorMessage
+                      className="text-red-600"
+                      name="phone"
+                      component={'span'}
+                    />
+                  </div>
+                </div>
+                <div className="flex w-full flex-col">
+                  <Field
+                    className="w-full rounded-md border border-white bg-inherit px-2 py-3 text-white"
+                    name="message"
+                    style={{ resize: 'none' }}
+                    as="textarea"
+                    placeholder="Коментар"
+                    columns={8}
+                    rows={5}
+                  />
+                  <ErrorMessage
+                    className="text-red-600"
+                    name="message"
+                    component={'span'}
+                  />
+                </div>
+                <button className="bg-custom-contactbtn rounded-md px-6 py-2 text-2xl font-medium tracking-wide text-white hover:bg-white hover:text-black hover:duration-700">
+                  Надіслати
+                </button>
+              </Form>
+            )}
           </Formik>
         </div>
       </div>
