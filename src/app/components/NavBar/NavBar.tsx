9d24a6cbbae799f0da6f7b9f6f3e3e9dc87bf487
sashakter <149799726+alexsnowll@users.noreply.github.com>
2024-10-09 09:28:33 +0200

    fix nav bar 1024

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -21,7 +21,7 @@ const NavBar: React.FC = () => {
   }, [burger])
   return (
     <nav
-      className={`absolute inset-0 z-50 flex w-full items-center h-28 justify-between px-6 py-6 text-xl uppercase text-white lg:px-4 lg:py-10 xl:px-12 2xl:text-2xl`}
+      className={`absolute inset-0 z-50 flex h-28 w-full items-center justify-between px-6 py-6 text-xl uppercase text-white lg:px-4 lg:py-10 xl:px-12 2xl:text-2xl`}
     >
       <Link className={`link ${pathname === '/' ? 'active' : ''}`} href="/">
         <Image
@@ -35,33 +35,36 @@ const NavBar: React.FC = () => {
       </Link>
       <div className="xl: flex items-center gap-10 pr-14 xl:justify-between">
         <div className="hidden gap-2 lg:flex xl:gap-6">
-          <Link className={`link ${pathname === '/' ? 'active' : ''} px-2 py-1 relative after:w-0 after:h-[1px] after:absolute after:bottom-0 after:left-0 after:bg-white hover:after:w-full hover:after:duration-200`} href="/">
+          <Link
+            className={`link ${pathname === '/' ? 'active' : ''} relative px-2 py-1 after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-0 after:bg-white hover:after:w-full hover:after:duration-200 lg:text-sm xl:text-xl`}
+            href="/"
+          >
             Головна
           </Link>
-          <Image src={'/ear.svg'} alt='Ear image' width={20} height={20}/>
+          <Image src={'/ear.svg'} alt="Ear image" width={20} height={20} />
           <Link
-            className={`link ${pathname === '/products' ? 'active' : ''} px-2 py-1 relative after:w-0 after:h-[1px] after:absolute after:bottom-0 after:left-0 after:bg-white hover:after:w-full hover:after:duration-200`}
+            className={`link ${pathname === '/products' ? 'active' : ''} relative px-2 py-1 after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-0 after:bg-white hover:after:w-full hover:after:duration-200 lg:text-sm xl:text-xl`}
             href="/products"
           >
             Продукція
           </Link>
-          <Image src={'/ear.svg'} alt='Ear image' width={20} height={20}/>
+          <Image src={'/ear.svg'} alt="Ear image" width={20} height={20} />
           <Link
-            className={`link ${pathname === '/partners' ? 'active' : ''} px-2 py-1 relative after:w-0 after:h-[1px] after:absolute after:bottom-0 after:left-0 after:bg-white hover:after:w-full hover:after:duration-200`}
+            className={`link ${pathname === '/partners' ? 'active' : ''} relative px-2 py-1 after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-0 after:bg-white hover:after:w-full hover:after:duration-200 lg:text-sm xl:text-xl`}
             href="/partners"
           >
             Партнерство
           </Link>
-          <Image src={'/ear.svg'} alt='Ear image' width={20} height={20}/>
+          <Image src={'/ear.svg'} alt="Ear image" width={20} height={20} />
           <Link
-            className={`link ${pathname === '/news' ? 'active' : ''} px-2 py-1 relative after:w-0 after:h-[1px] after:absolute after:bottom-0 after:left-0 after:bg-white hover:after:w-full hover:after:duration-200`}
+            className={`link ${pathname === '/news' ? 'active' : ''} relative px-2 py-1 after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-0 after:bg-white hover:after:w-full hover:after:duration-200 lg:text-sm xl:text-xl`}
             href="/news"
           >
             Новини
           </Link>
-          <Image src={'/ear.svg'} alt='Ear image' width={20} height={20}/>
+          <Image src={'/ear.svg'} alt="Ear image" width={20} height={20} />
           <Link
-            className={`link ${pathname === '/contacts' ? 'active' : ''} px-2 py-1 relative after:w-0 after:h-[1px] after:absolute after:bottom-0 after:left-0 after:bg-white hover:after:w-full hover:after:duration-200`}
+            className={`link ${pathname === '/contacts' ? 'active' : ''} relative px-2 py-1 after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-0 after:bg-white hover:after:w-full hover:after:duration-200 lg:text-sm xl:text-xl`}
             href="/contacts"
           >
             Контакти
