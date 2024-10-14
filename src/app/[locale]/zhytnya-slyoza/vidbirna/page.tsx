4b8b48415f5df96987c745b63038a23899024baf
sashakter <149799726+alexsnowll@users.noreply.github.com>
2024-10-14 03:16:58 +0200

    make fix with route

--- src/app/[locale]/zhytnya-slyoza/vidbirna/page.tsx
+++ src/app/[locale]/zhytnya-slyoza/vidbirna/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import Image from 'next/image'
-import SwiperProducts from '../../components/SwiperProducts/SwiperProducts'
+import SwiperProducts from '@/app/components/SwiperProducts/SwiperProducts'
 
 const Capacities: React.FC = () => {
   return (
