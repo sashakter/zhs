5d78ad341ab47961b3f8b914edde085f97d94cb3
sashakter <149799726+alexsnowll@users.noreply.github.com>
2024-10-15 12:34:23 +0200

    change adress

--- src/app/[locale]/contacts/page.tsx
+++ src/app/[locale]/contacts/page.tsx
@@ -14,13 +14,13 @@ export default function ContactPage() {
       <div className="bg-contactBackground bg-cover bg-no-repeat">
         <Contacts />
       </div>
-      <div>
+      <div className="w-full">
         <iframe
-          className="grayscale"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2540.837683488433!2d30.493611776097516!3d50.44412398781984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cef37e4b9011%3A0x85003f5b654854f6!2z0YPQuy4g0KHQsNC60YHQsNCz0LDQvdGB0LrQvtCz0L4sIDEzOSwg0JrQuNC10LIsIDAyMDAw!5e0!3m2!1sru!2sua!4v1726918074205!5m2!1sru!2sua"
-          width="600"
-          height="450"
-          style={{ border: '0', width: '100%' }}
+          className="w-full grayscale"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2541.292113464605!2d30.51172957613349!3d50.43565938842914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cefcf11a1f0d%3A0xd3e4b6775925d4bf!2sSaksahanskoho%20St%2C%2029%2C%20Kyiv%2C%20Ukraine%2C%2002000!5e0!3m2!1sen!2shu!4v1728988290957!5m2!1sen!2shu"
+          width="800"
+          height="600"
+          style={{ border: 0 }}
           allowFullScreen={false}
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
