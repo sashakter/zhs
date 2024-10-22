521d53007b8c15b4e4cbe29401961b826174a506
sashakter <149799726+alexsnowll@users.noreply.github.com>
2024-10-23 00:35:56 +0200

    translate agemodal

--- src/app/components/AgeVerificationModal.tsx
+++ src/app/components/AgeVerificationModal.tsx
@@ -1,9 +1,11 @@
 'use client'
+import { Link } from '@/navigation'
+import { useTranslations } from 'next-intl'
 import React, { useState, useEffect } from 'react'
 
 const AgeVerificationModal: React.FC = () => {
   const [isVisible, setIsVisible] = useState(true)
-
+  const t = useTranslations('AgeModal')
   useEffect(() => {
     const isVerified = localStorage.getItem('ageVerified')
     if (isVerified === 'true') {
@@ -25,32 +27,35 @@ const AgeVerificationModal: React.FC = () => {
   }
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80 uppercase">
       <div className="rounded-lg border border-white bg-black p-8 text-center text-white">
         <h2 className="mb-4 text-lg font-semibold">
-          ДАНИЙ КОНТЕНТ ДОСТУПНИЙ ТІЛЬКИ ПОВНОЛІТНІМ КОРИСТУВАЧАМ.
-          <br />
-          ВИ ДОСЯГЛИ ВІКУ ЛЕГАЛЬНОГО СПОЖИВАННЯ АЛКОГОЛЬНИХ НАПОЇВ?
+          {t.rich('title', {
+            br: () => <br />,
+          })}
         </h2>
-        <div className="flex justify-center gap-4">
+        <div className="flex justify-center gap-4 uppercase">
           <button
             onClick={handleYes}
-            className="rounded border border-white px-4 py-2 text-lg"
+            className="rounded border border-white px-4 py-2 text-lg uppercase"
           >
-            ТАК
+            {t('button')}
           </button>
           <button
             onClick={handleNo}
-            className="rounded border border-white px-4 py-2 text-lg"
+            className="rounded border border-white px-4 py-2 text-lg uppercase"
           >
-            НІ
+            {t('button2')}
           </button>
         </div>
         <p className="mt-4 text-sm">
-          ПІДТВЕРДЖУЮЧИ ВАШ ВІК ВИ ПОГОДЖУЄТЕСЬ З НАШОЮ
-          <a href="/privacy-policy" className="ml-1 underline">
-            ПОЛІТИКОЮ КОНФІДЕНЦІЙНОСТІ
-          </a>
+          {t.rich('description', {
+            privacy: (chunks) => (
+              <Link href={'/policies'} className="ml-1 underline">
+                {chunks}
+              </Link>
+            ),
+          })}
         </p>
       </div>
     </div>
