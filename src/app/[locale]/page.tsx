71d68d59faa79b48bc09719d6c53c5b64eba7cc0
sashakter <sashakter@gmail.com>
2025-09-26 13:56:08 +0200

    chore: snapshot before deps update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,17 +1,9 @@
-import dynamic from 'next/dynamic'
 import AboutCompany from '../components/AboutCompany/AboutCompany'
 import HeaderVideo from '../components/HeaderVideo/HeaderVideo'
-import Benefits from '../components/Partners/Partners'
-import News from '../components/News'
 import OurBrands from '../components/OurBrands/OurBrands'
 import Capacities from '../components/Capacities/Capacities'
 import Partners from '../components/Partners/Partners'
-import RunningSroke from '../components/RunningStroke'
 import React from 'react'
-
-const Chooser = dynamic(() => import('./products/page'), {
-  ssr: false,
-})
 
 const Page: React.FC = () => {
   return (
